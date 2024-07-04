1fbbb3c88282d5bdb4cc1390089ded909bb1d2a7
Sarah Stenfert <sarah.stenfert@gmail.com>
2024-07-05 10:41:05 +1200

    Just deleted some empty lines

--- input_output/age.py
+++ input_output/age.py
@@ -5,9 +5,3 @@ print(f'In 10 years, you will be {(age) + 10} years old.')
 print(f'In 20 years, you will be {(age) + 20} years old.')
 print(f'In 30 years, you will be {(age) + 30} years old.')
 print(f'In 40 years, you will be {(age) + 40} years old.')
-
-
-
-
-
-
